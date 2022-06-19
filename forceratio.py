ffb1c768bdeec7f0a95a4a1c697130994b8536f0
TsXor <74352334+TsXor@users.noreply.github.com>
2022-06-19 13:34:31 +0800

    Update forceratio.py

--- forceratio.py
+++ forceratio.py
@@ -13,15 +13,16 @@ class window_monitor:
         self.winsize = (self.window_rect[2]-self.window_rect[0], self.window_rect[3]-self.window_rect[1])
         self.XYratio = self.winsize[0]/self.winsize[1]
         self.container_wnd_handle = tk_container_wnd.winfo_id()
-        print(self.container_wnd_handle)
-        print(self.target_wnd_handle)
+        #print(self.container_wnd_handle)
+        #print(self.target_wnd_handle)
         tk_container_wnd.title(window_name)
         tk_container_wnd.wm_title(window_name)
         tk_container_wnd.bind('<Configure>', self.window_resize)
-        #夺舍
+        #print('夺舍')
         tk_container_wnd.geometry(str(self.winsize[0])+'x'+str(self.winsize[1])+'+'+str(self.window_rect[0])+'+'+str(self.window_rect[1]))
         win32gui.SetParent(self.target_wnd_handle, self.container_wnd_handle)
         #win32gui.ShowWindow(self.target_wnd_handle, win32con.SW_MAXIMIZE)
+        win32gui.SetWindowPos(self.target_wnd_handle, 0, 0, 0, self.winsize[0], self.winsize[1], win32con.SWP_NOZORDER)
         self.window_width = self.winsize[0]
         self.window_height = self.winsize[1]
     def window_resize(self, event=None):
@@ -36,11 +37,11 @@ class window_monitor:
             return
         if self.winsize[0] == self.last_winsize[0] and self.winsize[1] == self.last_winsize[1]:
             return
-        print(self.last_window_rect)
-        print(self.window_rect)
-        print(self.last_winsize)
-        print(self.winsize)
+        #print(self.last_window_rect)
+        #print(self.window_rect)
+        #print(self.last_winsize)
+        #print(self.winsize)
         target_size = (round(min(self.winsize[0], self.winsize[1]*self.XYratio)), round(min(self.winsize[0], self.winsize[1]*self.XYratio)/self.XYratio))
-        print(target_size)
+        #print(target_size)
         paramqueue = [round((-self.window_rect[0]+self.window_rect[2]-target_size[0])/2), round((-self.window_rect[1]+self.window_rect[3]-target_size[1])/2), target_size[0], target_size[1]]
         win32gui.SetWindowPos(target, 0, paramqueue[0], paramqueue[1], paramqueue[2], paramqueue[3], win32con.SWP_NOZORDER)
